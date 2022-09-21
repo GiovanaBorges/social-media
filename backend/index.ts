d2c161b7a640ec333db1dd15d03ee52fe50805e3
GiovanaBorges <gigi12345as17@gmail.com>
2022-09-20 23:09:53 -0300

    inital commit

---
+++ backend/index.ts
@@ -0,0 +1,8 @@
+require("dotenv").config()
+const express = require("express")
+const server = express()
+server.use(express.json())
+
+server.listen(8087,()=>{
+    console.log("Server is running")
+})
